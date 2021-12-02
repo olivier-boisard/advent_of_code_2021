1349436f4b862e1c94dba0cc2a2727c9e098115e
Olivier Boisard <olivier.boisard@gmail.com>
2021-12-02 07:23:00 +0100

    Fixed path to files.

--- src/advent2021/day01/part1.py
+++ src/advent2021/day01/part1.py
@@ -1,9 +1,8 @@
 import numpy as np
-import os
 
 
 def _main():
-    with open(os.path.join('day01', 'input.txt')) as f:
+    with open('input.txt') as f:
         puzzle_input = [int(line) for line in f.readlines()]
     n_increases = (np.diff(puzzle_input) > 0).sum()
     print(f'Number of increases: {n_increases}')
--- src/advent2021/day01/part2.py
+++ src/advent2021/day01/part2.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import os
 
 
 def _main():
-    with open(os.path.join('day01', 'input.txt')) as f:
+    with open('input.txt') as f:
         puzzle_input = [int(line) for line in f.readlines()]
     n_increases = (pd.Series(puzzle_input).rolling(3).sum().dropna().diff() > 0).sum()
     print(f'Number of increases: {n_increases}')
