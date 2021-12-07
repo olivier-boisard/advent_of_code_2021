c9070b657854209a22333e48064b68c3cda50509
Olivier Boisard <olivier.boisard@gmail.com>
2021-12-07 07:42:31 +0100

    Removed useless variable.

--- src/advent2021/day07/part2.py
+++ src/advent2021/day07/part2.py
@@ -5,7 +5,6 @@ def _main():
     with open('input.txt') as f:
         horizontal_positions = np.array([int(n) for n in f.readlines()[0].split(',')], dtype=int)
 
-    best_position = None
     min_cost_in_fuel = None
     for position in range(np.min(horizontal_positions), np.max(horizontal_positions) + 1):
         distances = np.abs(horizontal_positions - position)
@@ -13,7 +12,6 @@ def _main():
         if min_cost_in_fuel is None:
             min_cost_in_fuel = cost_in_fuel
         elif cost_in_fuel < min_cost_in_fuel:
-            best_position = position
             min_cost_in_fuel = cost_in_fuel
 
     print(min_cost_in_fuel)
