b586f303caa0b29c775be27035bb1e54d29890d0
Olivier Boisard <olivier.boisard@gmail.com>
2021-12-13 08:19:50 +0100

    Continued day 12.

--- src/advent2021/day12/part2.py
+++ src/advent2021/day12/part2.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Set, Dict
+from typing import Set
 
 
 def _main():
@@ -12,15 +12,14 @@ def _main():
 class Cave:
     is_big: bool
     connected_caves: Set[str] = field(default_factory=set)
-    visited: bool = False
+    n_visits: int = 0
 
-    @property
-    def can_be_visited(self):
-        return self.is_big or not self.visited
+    def can_be_visited(self, is_single_small_cave_visited_twice):
+        return self.is_big or (not self.n_visits == 0) or (not is_single_small_cave_visited_twice and self.n_visits < 2)
 
 
 def _load_puzzle_input():
-    with open('input.txt') as f:
+    with open('example1.txt') as f:
         connections = [line.strip() for line in f.readlines()]
     return connections
 
@@ -40,16 +39,17 @@ def _add_caves_to_graph(cave_in, cave_out, graph):
     graph[cave_in].connected_caves.add(cave_out)
 
 
-def _walk_graph(graph, start_cave):
+def _walk_graph(graph, start_cave, is_single_small_cave_visited_twice=False):
     if start_cave == 'end':
         return 1
     n_paths = 0
     current_cave = graph[start_cave]
-    current_cave.visited = True
+    current_cave.n_visits += 1
+    is_single_small_cave_visited_twice |= not current_cave.is_big and current_cave.n_visits == 2
     for next_cave in sorted(current_cave.connected_caves):
-        if graph[next_cave].can_be_visited:
+        if graph[next_cave].can_be_visited(is_single_small_cave_visited_twice):
             n_paths += _walk_graph(graph, next_cave)
-    current_cave.visited = False
+    current_cave.n_visits -= 1
     return n_paths
 
 
